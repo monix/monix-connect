1dd523cf35b3ff316457320b514ebaeb5dabfaf6
Pau <pau.alarcon.b@gmail.com>
2020-04-13 13:46:19 +0200

    First working implementation of multipart upload

--- s3/src/it/scala/scalona/monix/connect/s3/S3AsyncSpec.scala
+++ s3/src/it/scala/scalona/monix/connect/s3/S3AsyncSpec.scala
@@ -1,23 +1,19 @@
 package scalona.monix.connect.s3
 
 import java.nio.ByteBuffer
-import java.util.concurrent.CompletableFuture
 
-import com.amazonaws.services.s3.model.{ Grant, Owner }
 import monix.eval.Task
 import org.scalacheck.Gen
-import org.scalatest.{ BeforeAndAfterAll, Matchers, WordSpecLike }
-import software.amazon.awssdk.services.s3.model.{ CreateBucketRequest, GetObjectAclResponse, PutObjectRequest, PutObjectResponse }
+import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
+import software.amazon.awssdk.services.s3.model.{CompleteMultipartUploadResponse, PutObjectResponse}
 
-import scala.concurrent.{ Await, Future }
 import scala.concurrent.duration._
 import scala.jdk.FutureConverters._
 import monix.execution.Scheduler.Implicits.global
 import monix.reactive.Observable
-import org.scalatest.concurrent.{ Eventually, ScalaFutures }
-import software.amazon.awssdk.core.internal.async.ByteArrayAsyncResponseTransformer
+import org.scalatest.concurrent.{Eventually, ScalaFutures}
 
-import scala.util.Try
+import scala.util.{Failure, Success, Try}
 class S3AsyncSpec
   extends WordSpecLike with Matchers with BeforeAndAfterAll with ScalaFutures with S3Fixture with Eventually {
 
@@ -39,6 +35,7 @@ class S3AsyncSpec
           //when
           val t: Task[PutObjectResponse] = S3.putObject(bucketName, key, ByteBuffer.wrap(content.getBytes()))
 
+          //then
           whenReady(t.runToFuture) { putResponse =>
             val s3Object: ByteBuffer = S3.getObject(bucketName, key).runSyncUnsafe()
             putResponse shouldBe a[PutObjectResponse]
@@ -59,6 +56,7 @@ class S3AsyncSpec
             Some(content.length),
             Some("appliction/json"))
 
+          //then
           whenReady(t.runToFuture) { putResponse =>
             val s3Object: ByteBuffer = S3.getObject(bucketName, key).runSyncUnsafe()
             putResponse shouldBe a[PutObjectResponse]
@@ -66,75 +64,57 @@ class S3AsyncSpec
           }
         }
       }
-
-      s"downloads a ByteBuffer of an existing s3 object" in {
-        //given
-        val key = Gen.alphaLowerStr.sample.get
-        val content = Gen.alphaUpperStr.sample.get
-        s3SyncClient.putObject(bucketName, key, content)
-
-        //when
-        val t: Task[ByteBuffer] = S3.getObject(bucketName, key)
-
-        whenReady(t.runToFuture) { actualContent: ByteBuffer =>
-          s3SyncClient.doesObjectExist(bucketName, key) shouldBe true
-          actualContent shouldBe a[ByteBuffer]
-          actualContent.array() shouldBe content.getBytes()
-        }
-      }
-
     }
-    /*"correctly upload a single S3Object to S3" in {
-      //given
-      val key: String = Gen.alphaLowerStr.sample.get
-      val content: String = Gen.alphaLowerStr.sample.get
-      val s3Sink: Consumer[S3Object, Either[Throwable, PutObjectResult]] = S3.putObject
-
-      //when
-      val maybePutResult: Either[Throwable, PutObjectResult] = {
-        Observable
-          .fromIterable(List(S3Object(bucketName, key, content)))
-          .consumeWith(s3Sink)
-          .runSyncUnsafe()
-      }
-
-      //then
-      maybePutResult.isRight shouldBe true
-      maybePutResult.right.get shouldBe a[PutObjectResult]
-      //val actualContent: String = S3Client().getObjectAsString(bucketName, key) todo check s3 file content
-      //content shouldBe actualContent
-    }*/
   }
 
-  /*"correctly upload a collection of S3Objects in S3" in {
-    //given
-    val key: String = Gen.alphaLowerStr.sample.get
-    val content: String = Gen.alphaLowerStr.sample.get
-    val s3Sink: Consumer[S3Object, Either[Throwable, PutObjectResult]] = S3.putObject
+  it should {
+    "perform multipart upload" in {
+      //given
+      val key = Gen.alphaLowerStr.sample.get
+      val content = ByteBuffer.wrap(Gen.alphaUpperStr.sample.get.getBytes)
+      val stream = Observable.fromIterable(Seq(content))
 
-    //when
-    val maybePutResult: Either[Throwable, PutObjectResult] = {
-      Observable
-        .fromIterable(List(S3Object(bucketName, key, content)))
-        .consumeWith(s3Sink)
-        .runSyncUnsafe()
+      //when
+      val t: Task[CompleteMultipartUploadResponse] = S3.multipartUpload(bucketName, key, stream)
+
+      //then
+      whenReady(t.runToFuture) { completeMultipartUpload =>
+        val s3Object: ByteBuffer = S3.getObject(bucketName, key).runSyncUnsafe()
+        s3SyncClient.doesObjectExist(bucketName, key) shouldBe true
+        completeMultipartUpload shouldBe a[CompleteMultipartUploadResponse]
+        s3Object.array() shouldBe content.array()
+      }
     }
+  }
 
-    //then
-    maybePutResult.isRight shouldBe true
-    maybePutResult.right.get shouldBe a[PutObjectResult]
-    //val actualContent: String = S3Client().getObjectAsString(bucketName, key) todo check s3 file content
-    //content shouldBe actualContent
-  }*/
+  it should {
+    "download a ByteBuffer of an existing s3 object" in {
+      //given
+      val key = Gen.alphaLowerStr.sample.get
+      val content = Gen.alphaUpperStr.sample.get
+      s3SyncClient.putObject(bucketName, key, content)
+
+      //when
+      val t: Task[ByteBuffer] = S3.getObject(bucketName, key)
+
+      whenReady(t.runToFuture) { actualContent: ByteBuffer =>
+        s3SyncClient.doesObjectExist(bucketName, key) shouldBe true
+        actualContent shouldBe a[ByteBuffer]
+        actualContent.array() shouldBe content.getBytes()
+      }
+    }
+  }
 
   override def beforeAll(): Unit = {
     super.beforeAll()
-    //val createBucketRequest: CreateBucketRequest = CreateBucketRequest.builder().bucket(bucketName).build()
-    //Await.result(s3Client.createBucket(createBucketRequest).asScala, 5.seconds)
-    Try(s3SyncClient.createBucket(bucketName))
+    Try(s3SyncClient.createBucket(bucketName)) match {
+      case Failure(exception) => info(s"The attempt to create bucket $bucketName failed since it already existed, exception: $exception")
+      case Success(_) => info(s"Bucket $bucketName created successfully")
+    }
   }
 
   override def afterAll(): Unit = {
     super.afterAll()
+    s3SyncClient.deleteBucket(bucketName)
   }
 }
--- s3/src/main/scala/scalona/monix/connect/s3/S3.scala
+++ s3/src/main/scala/scalona/monix/connect/s3/S3.scala
@@ -3,14 +3,16 @@ package scalona.monix.connect.s3
 import java.nio.ByteBuffer
 import java.util.concurrent.CompletableFuture
 
-import monix.reactive.{ Consumer, Observable, Observer }
-import monix.execution.{ Ack, Scheduler }
+import monix.reactive.{Consumer, Observable, Observer}
+import monix.execution.{Ack, Scheduler}
 import monix.eval.Task
-import software.amazon.awssdk.core.async.{ AsyncRequestBody, AsyncResponseTransformer, SdkPublisher }
+import software.amazon.awssdk.core.async.{AsyncRequestBody, AsyncResponseTransformer, SdkPublisher}
 import software.amazon.awssdk.services.s3.S3AsyncClient
-import software.amazon.awssdk.services.s3.model.{ GetObjectAclRequest, GetObjectAclResponse, GetObjectRequest, GetObjectResponse, PutObjectRequest, PutObjectResponse }
+import software.amazon.awssdk.services.s3.model.{CompleteMultipartUploadRequest, CompleteMultipartUploadResponse, CompletedMultipartUpload, CompletedPart, CreateMultipartUploadRequest, GetObjectAclRequest, GetObjectAclResponse, GetObjectRequest, GetObjectResponse, PutObjectRequest, PutObjectResponse, UploadPartRequest, UploadPartResponse}
 
+import scala.concurrent.Future
 import scala.jdk.FutureConverters._
+import scala.jdk.CollectionConverters._
 
 object S3 {
 
@@ -33,6 +35,67 @@ object S3 {
     Task.fromFuture(
       s3Client.putObject(putObjectRequest, requestBody).asScala
     )
+  }
+
+  def multipartUpload(bucketName: String, key: String, contentStream: Observable[ByteBuffer], contentType: Option[String] = None)(
+    implicit s3Client: S3AsyncClient,
+    s: Scheduler): Task[CompleteMultipartUploadResponse] = {
+    val multiPartUploadrequest = CreateMultipartUploadRequest
+      .builder()
+      .bucket(bucketName)
+      .contentType(contentType.getOrElse("plain/text"))
+      .key(key)
+      .build()
+
+    val futureUploadId =
+      Task.fromFuture(s3Client.createMultipartUpload(multiPartUploadrequest).asScala.map(_.uploadId()))
+    futureUploadId.flatMap { uploadId =>
+      val t: Task[List[CompletedPart]] = contentStream
+        .mapAccumulate[Int, (Int, ByteBuffer)](0)((acc, bytes) => (acc + 1, (acc, bytes)))
+        .map {
+          case (partNumber, byteBuffer: ByteBuffer) =>
+            val uploadPartRequest = UploadPartRequest
+              .builder()
+              .bucket(bucketName)
+              .key(key)
+              .partNumber(partNumber)
+              .uploadId(uploadId)
+              .contentLength(byteBuffer.array().length.toLong)
+              .build()
+            val asyncRequestBody = AsyncRequestBody.fromBytes(byteBuffer.array())
+            Task
+              .fromFuture(
+                s3Client
+                  .uploadPart(
+                    uploadPartRequest,
+                    asyncRequestBody
+                  )
+                  .asScala)
+              .map(resp => CompletedPart.builder().partNumber(partNumber).eTag(resp.eTag()).build())
+        }
+        .toListL
+        .flatMap(Task.sequence(_))
+
+      val completeMultipartUploadResp: Task[CompleteMultipartUploadResponse] = t.flatMap { completedParts =>
+        val completedMultipartUpload = CompletedMultipartUpload.builder().parts(completedParts.asJava).build()
+        Task.fromFuture(
+          s3Client
+            .completeMultipartUpload(
+              CompleteMultipartUploadRequest
+                .builder()
+                .bucket(bucketName)
+                .key(key)
+                .uploadId(uploadId)
+                .multipartUpload(completedMultipartUpload)
+                .build()
+            )
+            .asScala
+        )
+      }
+
+      completeMultipartUploadResp
+    }
+
   }
 
   def getObject(bucketName: String, key: String)(implicit s3Client: S3AsyncClient): Task[ByteBuffer] = {
