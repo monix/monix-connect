5322780476609e11d19f97f78b6d4cf048094d66
Pau <pau.alarcon.b@gmail.com>
2020-04-22 22:39:00 +0200

    Fixed start dependencies script

--- start-dependencies.sh
+++ start-dependencies.sh
@@ -5,8 +5,8 @@ set -e
 CURRENT_DIR=$(pwd)
 echo "CURRENT_DIR=$CURRENT_DIR"
 
-docker stop $(docker ps -a -q)
-docker rm $(docker ps -a -q)
+docker-compose -f ./docker-compose.yml stop minio
+docker-compose -f ./docker-compose.yml rm -f minio
 docker-compose -f docker-compose.yml up -d minio localstack
 
 sleep 40
