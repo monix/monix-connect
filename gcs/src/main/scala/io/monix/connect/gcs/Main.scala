212c836862c6664fa54164631bcd8e483d9757ef
Richard <someoneoutthere2009@live.com>
2020-04-25 22:35:53 +0200

    Removed Name

--- gcs/src/main/scala/io/monix/connect/gcs/Main.scala
+++
@@ -1,9 +0,0 @@
-package io.monix.connect.gcs
-
-import io.monix.connect.gcs.configuration.BucketConfig
-import monix.eval.Task
-
-object Main {
-  val config: BucketConfig = BucketConfig("mybucket")
-  val bucket: Task[Bucket] = Bucket(config)
-}
