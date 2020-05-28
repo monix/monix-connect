1c767da091b98ccf87f14fb721f17cb31f01a2fe
Scala Steward <me@scala-steward.org>
2020-05-28 10:44:38 +0200

    Update monix-reactive to 3.2.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
     val DynamoDb = "2.10.60"
     val PureConfig = "0.12.3"
     val S3 = "2.10.91"
-    val Monix = "3.2.0"
+    val Monix = "3.2.1"
     val AkkaStreams = "2.6.4"
     val Hadoop = "3.1.3"
 
