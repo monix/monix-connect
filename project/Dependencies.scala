76bde134cfee16dfc9c492bf4dc1305e3cb618e6
Scala Steward <me@scala-steward.org>
2020-05-28 10:44:43 +0200

    Update hadoop-client, hadoop-common, ... to 3.2.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
     val S3 = "2.10.91"
     val Monix = "3.2.0"
     val AkkaStreams = "2.6.4"
-    val Hadoop = "3.1.3"
+    val Hadoop = "3.2.1"
 
     //test
     val Scalatest = "3.1.2"
