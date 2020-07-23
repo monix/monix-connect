3b89412372d6d5a8cec11c55174ff3667b74eebb
Scala Steward <me@scala-steward.org>
2020-07-24 00:36:53 +0200

    Update scalacheck to 1.14.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,7 +14,7 @@ object Dependencies {
     val Cats_Effect = "2.1.3"
     //test
     val Scalatest = "3.1.2"
-    val Scalacheck = "1.14.0"
+    val Scalacheck = "1.14.3"
     val Mockito = "1.14.3"
     val Cats = "2.0.0"
     val GCNio = "0.121.2"
