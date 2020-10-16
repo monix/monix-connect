df43f809803792dff981fe93798a1898a5cb2832
Pau <pau.alarcon.b@gmail.com>
2020-10-16 08:54:42 +0200

    Add config fail scenarios

--- aws-auth/src/test/scala/monix/connect/aws/auth/AwsClientConfigSpec.scala
+++ aws-auth/src/test/scala/monix/connect/aws/auth/AwsClientConfigSpec.scala
@@ -25,8 +25,11 @@ import pureconfig._
 import pureconfig.generic.auto._
 import MonixAwsConf.Implicits._
 import org.scalatest.matchers.should.Matchers
+import pureconfig.error.ConfigReaderException
 import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider
 import software.amazon.awssdk.regions.Region
+
+import scala.util.{Failure, Try}
 
 class AwsClientConfigSpec extends AnyFlatSpec with Matchers {
 
@@ -36,11 +39,37 @@ class AwsClientConfigSpec extends AnyFlatSpec with Matchers {
 
     //then
     awsClientConf.monixAws.credentials shouldBe a[DefaultCredentialsProvider]
-    awsClientConf.monixAws.endpoint shouldBe Some(URI.create("localhost:4566"))
+    awsClientConf.monixAws.endpoint.isDefined shouldBe false //Some(URI.create("localhost:4566"))
     awsClientConf.monixAws.region shouldBe Region.AWS_GLOBAL
   }
 
   it should "not require endpoint nor http client settings" in {
+    //given
+    val configSource = ConfigSource.string(
+      "" +
+        s"""
+           |{
+           |  monix-aws: {
+           |    credentials: {
+           |      provider: "default"
+           |    }
+           |    region: "aws-global"
+           |    endpoint: "localhost:4566"
+           |  }
+           |}
+           |""".stripMargin)
+
+    //when
+    val awsClientConf = configSource.loadOrThrow[AppConf]
+
+    //then
+    awsClientConf.monixAws.credentials shouldBe a[DefaultCredentialsProvider]
+    awsClientConf.monixAws.endpoint shouldBe Some(URI.create("localhost:4566"))
+    awsClientConf.monixAws.httpClient.isDefined shouldBe false
+    awsClientConf.monixAws.region shouldBe Region.AWS_GLOBAL
+  }
+
+  it should "read the local endpoint as a uri" in {
     //given
     val configSource = ConfigSource.string(
       "" +
@@ -63,6 +92,48 @@ class AwsClientConfigSpec extends AnyFlatSpec with Matchers {
     awsClientConf.monixAws.endpoint.isDefined shouldBe false
     awsClientConf.monixAws.httpClient.isDefined shouldBe false
     awsClientConf.monixAws.region shouldBe Region.AWS_GLOBAL
+  }
+
+  it should "fail when credentials are not present" in {
+    //given
+    val configSource = ConfigSource.string(
+      "" +
+        s"""
+           |{
+           |  monix-aws: {
+           |    region: "aws-global"
+           |  }
+           |}
+           |""".stripMargin)
+
+    //when
+    val awsClientConf = Try(configSource.loadOrThrow[AppConf])
+
+    //then
+    awsClientConf.isFailure shouldBe true
+    awsClientConf shouldBe a[Failure[ConfigReaderException[AppConf]]]
+  }
+
+  it should "fail when credentials region is not present" in {
+    //given
+    val configSource = ConfigSource.string(
+      "" +
+        s"""
+           |{
+           |  monix-aws: {
+           |    credentials: {
+           |      provider: "default"
+           |    }
+           |  }
+           |}
+           |""".stripMargin)
+
+    //when
+    val awsClientConf = Try(configSource.loadOrThrow[AppConf])
+
+    //then
+    awsClientConf.isFailure shouldBe true
+    awsClientConf shouldBe a[Failure[ConfigReaderException[AppConf]]]
   }
 
 }
