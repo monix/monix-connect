59f976e8fda17ae5f021bb4d103ab12795f06e96
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-10-16 11:45:54 +0200

    Update mongodb-driver-reactivestreams to 4.1.1 (#381)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,7 @@ object Dependencies {
     val Hadoop = "3.1.4"
     val Monix = "3.2.0"
     val MongoScala = "2.9.0"
-    val MongoReactiveStreams = "4.1.0"
+    val MongoReactiveStreams = "4.1.1"
     val S3 = "2.14.21"
     val Parquet = "1.11.1"
 
