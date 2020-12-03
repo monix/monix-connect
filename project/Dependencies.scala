78bb15fe737fd5fdaf352c10fe43d7e93d07d3e1
Scala Steward <me@scala-steward.org>
2020-12-04 00:31:21 +0100

    Update auth, dynamodb, s3 to 2.15.39

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
 
     //main
     val Monix = "3.3.0"
-    val AwsSdk = "2.15.35"
+    val AwsSdk = "2.15.39"
     val AkkaStreams = "2.6.9"
     val AWS = "1.11.749"
     val Cats_Effect = "2.1.3"
