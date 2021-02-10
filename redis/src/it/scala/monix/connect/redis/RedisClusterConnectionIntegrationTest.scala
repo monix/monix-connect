4315334bcdcf936e428cc3732aff048ab7537d8e
Pau <pau.alarcon.b@gmail.com>
2021-02-10 22:56:55 +0100

    Cluster successfully connected

--- redis/src/it/scala/monix/connect/redis/RedisClusterConnectionIntegrationTest.scala
+++ redis/src/it/scala/monix/connect/redis/RedisClusterConnectionIntegrationTest.scala
@@ -17,7 +17,12 @@ class RedisClusterConnectionIntegrationTest extends AnyFlatSpec with RedisIntegr
   override implicit val patienceConfig: PatienceConfig = PatienceConfig(4.seconds, 100.milliseconds)
 
   val clusterRedisUri = List("redis://localhost:7001", "redis://localhost:7002", "redis://localhost:7003")
-  val clusterRedisUris = List(RedisUri("redis://localhost", 7000), RedisUri("redis://localhost", 7001), RedisUri("redis://localhost", 7002))
+  val clusterRedisUris = List(RedisUri("127.0.0.1", 7000),
+    RedisUri("127.0.0.1", 7001),
+    RedisUri("127.0.0.1", 7002),
+    RedisUri("127.0.0.1", 7003),
+    RedisUri("127.0.0.1", 7004),
+    RedisUri("127.0.0.1", 7005))
 
   "ClusterConnection" should "can connect to multiple uri" in {
     //given
