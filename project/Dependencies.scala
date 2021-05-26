cdabe4d1ab01e39e089362be90922850f985ad04
Scala Steward <me@scala-steward.org>
2021-05-26 02:38:08 +0200

    Update commons-io to 2.9.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -94,7 +94,7 @@ object Dependencies {
   val GCS = Seq(
     "com.google.cloud"               % "google-cloud-storage" % Versions.GCS,
     "com.google.cloud"               % "google-cloud-nio" % Versions.GCNio % IntegrationTest,
-    "commons-io"                     % "commons-io" % "2.6" % Test
+    "commons-io"                     % "commons-io" % "2.9.0" % Test
   ) ++ commonDependencies(hasIt = true)
 
   val Elasticsearch = Seq(
