efb56a5e2599dab934a2aa9138b2798ddb2a61d2
Scala Steward <me@scala-steward.org>
2021-09-18 02:08:41 +0200

    Update scalatest to 3.2.10

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -20,7 +20,7 @@ object Dependencies {
     val ScalaCompat = "2.5.0"
 
     //test
-    val Scalatest = "3.2.9"
+    val Scalatest = "3.2.10"
     val Scalacheck = "1.14.0"
     val Mockito = "1.15.0"
     val GCNio = "0.123.3"
