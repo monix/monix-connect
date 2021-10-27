1f64b7a31933c5bc82b7223bb6f3d23276fe8800
Scala Steward <me@scala-steward.org>
2021-10-27 08:55:46 +0200

    Update scalatest to 3.2.10

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -20,7 +20,7 @@ object Dependencies {
     val ScalaCompat = "2.5.0"
 
     //test
-    val Scalatest = "3.2.9"
+    val Scalatest = "3.2.10"
     val MonixTestingScalatest = "0.2.0"
     val Scalacheck = "1.14.0"
     val Mockito = "1.15.0"
