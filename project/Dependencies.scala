37faaa45a69b87e671839ebd521ae0d21726a916
Scala Steward <me@scala-steward.org>
2021-12-01 02:50:27 +0100

    Update auth, dynamodb, s3, sqs to 2.17.93

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
 
     //main
     val Monix = "3.4.0"
-    val AwsSdk = "2.17.66"
+    val AwsSdk = "2.17.93"
     val AkkaStreams = "2.6.9"
     val GCS = "1.107.0"
     val Hadoop = "3.3.1"
